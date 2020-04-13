50b10eebdae14e75c4cf4aa3e292095fdbe03269
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-04-12 22:18:14 -0700

    Add files via upload

--- solver.py
+++ solver.py
@@ -1,71 +1,63 @@
 import boards
-board = boards.board_medium
+
+board = boards.board_medium  # initialize grids (board and cells)
 cells = [[], [], [], [], [], [], [], [], []]
-corners = [3, 6, 9]
-
-
-def closest(lst, K):
-    return lst[min(range(len(lst)), key=lambda i: abs(lst[i] - K))]
+solved = 0
+runs = 0
+changed = True
 
 
 class Cell(object):
     def __init__(self, row, column, value):
-        global corners
         self.row = row
         self.column = column
         self.value = value
         self.possible = []
-        # self.box = [closest(corners, self.row) - 1, closest(corners, self.column) - 1]
-        self.box = [int(row / 3) * 3 + 2, int(column / 3) * 3 + 2]
+        self.box = [int(row / 3) * 3 + 2, int(column / 3) * 3 + 2]  # top left corner of sub-grid
 
-    def calc_possibles(self, grid):
+    def calc_possibles(self, grid):  # calculates all possible values for self
         self.possible.clear()
         if self.value == 0:
             for value in range(1, 10):
                 self.possible.append(value)
-            for cell in grid[self.row]:
+            for cell in grid[self.row]:  # checks row
                 if cell in self.possible:
                     self.possible.remove(cell)
-            for row in grid:
+            for row in grid:  # checks column
                 if row[self.column] in self.possible:
                     self.possible.remove(row[self.column])
-            for row in range(self.box[0] - 2, self.box[0] + 1):
+            for row in range(self.box[0] - 2, self.box[0] + 1):  # checks sub-grid
                 for column in range(self.box[1] - 2, self.box[1] + 1):
                     if grid[row][column] in self.possible:
                         self.possible.remove(grid[row][column])
-            print(" ")
 
 
-def create_cells(grid):
+def create_cells(grid):  # creates cell grid that matches given board
     for row in range(9):
         for column in range(9):
             base_value = grid[row][column]
             cells[row].append(Cell(row, column, base_value))
 
 
-def print_board():
+def print_board():  # prints board in correct order
     for row in cells:
         for cell in row:
             print(cell.value, end=' ')
         print("")
 
 
-def get_possibles(grid):
+def get_possibles(grid):  # gets possible values for the whole board
     for row in range(9):
         for column in range(9):
             cells[row][column].calc_possibles(grid)
 
 
-create_cells(board)
-for row in cells:
-    for cell in row:
-        print(cell.value, end=' ')
-    print("")
+def setup(grid):  # sets up grid and prints initial board
+    create_cells(grid)
+    print_board()
 
 
-def solve_unique():
-    global board, cells
-    count = 0
+def solve_unique():  # solves cells that instead of having one possible value, have a unique possible value
     found_row = 0
     found_col = 0
     for g_row in range(3):
@@ -84,14 +76,8 @@ def solve_unique():
                     board[found_row][found_col] = cells[found_row][found_col].value
 
 
-solved = 0
-runs = 0
-changed = True
-while changed:
-    # get_possibles(board)
-    get_possibles(board)
-    solve_unique()
-    changed = False
+def solve_simples():  # solves cells with only one possible value
+    global changed, solved
     for row in range(9):
         for column in range(9):
             cells[row][column].calc_possibles(board)
@@ -102,14 +88,15 @@ while changed:
                 solved += 1
                 changed = True
 
+
+setup(board)
+while changed: # main solving loop that breaks when the board doesn't change
+    get_possibles(board)
+    solve_unique()
+    changed = False
+    solve_simples()
     runs += 1
 
-print("\n \n \n")
-for row in cells:
-    for cell in row:
-        print(cell.value, end=' ')
-    print("")
-print(solved)
-print(runs)
-cells[0][8].calc_possibles(board)
-print(cells[0][8].possible)
+print("\nSolved board: ")
+print_board()
+print("solved", solved, "cells in", runs, "runs")
